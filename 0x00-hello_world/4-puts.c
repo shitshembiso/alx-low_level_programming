1ddab213d80a446696737103040666650a07b8cd
shitshembiso <ntlharhi74@gmail.com>
2022-09-12 16:01:54 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  *main- Serves as the entry point for the program
- *
+ *Description: function returns EOF if an error occurs
  *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
