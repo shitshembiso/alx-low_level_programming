11ca04a9ecf229d79e9b84b1bf374e7c33586316
shitshembiso <ntlharhi74@gmail.com>
2022-09-12 17:44:25 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
-
 /**
- *
- *main - print if the number is postive, zero, or negative
- *
- *Description: using the main function
- *this program prints programming is positive, 0 , or negative
- *
- *Return: 0
- *
- */
-
+ * *main - entry point
+ * *
+ * *Return: 0 after printing function
+ * */
 int main(void)
-
 {
-
-	int n;
-
-	for (n = '0'; n <= '9'; n++)
-
-	{
-
-			putchar(n);
-
-	}
-
-	putchar('\n');
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
