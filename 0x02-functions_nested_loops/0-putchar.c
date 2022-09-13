5ba89f9008ca14fd95a87a977444827493475930
shitshembiso <ntlharhi74@gmail.com>
2022-09-13 07:12:12 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * C program to demonstrate putchar() method
+ * Return: by default returns 0
+ * Description: program prints _putchar, followed by a new line
+ */
+int main(void)
+{
+	puts("\"_putchar" + "\n")
+		return (0);
+}	
